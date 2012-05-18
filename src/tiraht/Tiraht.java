669207b2767b3fa7caf2661177f445d05a98ea11
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-05-18 22:34:50 +0300

    Tee pääohjelmaan prototyyppitoteutus LZ78-pakkauksesta
    Tee sanakirjaa käyttäen testi kirjan mallin mukaan. Tulos on oikea,
    joten algoritmi lienee oikeansuuntainen!
    
    Huom! Koodi toimii väärin, jos lähtötekstin viimeiset symbolit
    löytyvät sanakirjasta. Tällöin nämä viimeiset symbolit jäävät
    koodaamatta kokonaan.

--- src/tiraht/Tiraht.java
+++ src/tiraht/Tiraht.java
@@ -2,6 +2,37 @@ package tiraht;
 
 public class Tiraht {
     public static void main(String[] args) {
-        // TODO code application logic here
+        /**
+         * Esimerkki kirjasta Fundamental Data Compression, s. 137-138.
+         */
+        String sourceStr = "a date at a date";
+        StringDict dict = new StringDictWithHashMap();
+        String key = "";
+        int lastToken = 0;
+        for (char c : sourceStr.toCharArray()) {
+            /*
+             * Ei yritetä etsiä tyhjää merkkijonoa sanakirjasta.
+             */
+            key += c;
+            int token = dict.search(key);
+            if (token != -1) {
+                /**
+                 * Symboli löytyi. Merkitään koodi muistiin ja yritetään
+                 * etsiä pitempää osumaa.
+                 */
+                lastToken = token;
+                continue;
+            }
+            /**
+             * Avainta ei löytynyt sanakirjasta.
+             */
+            token = lastToken;
+            int nextToken = dict.insert(key);
+            System.out.println("Tulosta (" + token + ", '" + c + "').");
+            System.out.println("    Lisää sana \"" + key + "\" koodilla " + nextToken + " sanakirjaan.");
+
+            key = "";
+            lastToken = 0;
+        }
     }
 }
