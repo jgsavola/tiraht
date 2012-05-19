65472a286f598a2aa8ab0fb121de89a9780b3205
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-05-19 18:44:37 +0300

    Käytä StringReaderiä lähtödatan lukemisessa
    Tämän pitäisi korjata bugi, jossa merkkijonon loppu jää koodaamatta ja
    tulostamatta, jos merkkijonon loppu löytyy sanakirjasta.

--- src/tiraht/Tiraht.java
+++ src/tiraht/Tiraht.java
@@ -1,38 +1,35 @@
 package tiraht;
 
+import java.io.IOException;
+import java.io.StringReader;
+
 public class Tiraht {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /**
          * Esimerkki kirjasta Fundamental Data Compression, s. 137-138.
          */
         String sourceStr = "a date at a date";
         StringDict dict = new StringDictWithHashMap();
-        String key = "";
-        int lastToken = 0;
-        for (char c : sourceStr.toCharArray()) {
-            /*
-             * Ei yritetä etsiä tyhjää merkkijonoa sanakirjasta.
-             */
-            key += c;
-            int token = dict.search(key);
-            if (token != -1) {
-                /**
-                 * Symboli löytyi. Merkitään koodi muistiin ja yritetään
-                 * etsiä pitempää osumaa.
-                 */
+        StringReader reader = new StringReader(sourceStr);
+        int symbol;
+        while ((symbol = reader.read()) != -1) {
+            String key = "";
+            int lastToken = 0;
+            int token = 0;
+            while ((token = dict.search(key + (char)symbol)) != -1) {
+                int nextSymbol = reader.read();
+                if (nextSymbol == -1)
+                    break;
+                key += (char)symbol;
                 lastToken = token;
-                continue;
+                symbol = nextSymbol;
             }
             /**
              * Avainta ei löytynyt sanakirjasta.
              */
-            token = lastToken;
-            int nextToken = dict.insert(key);
-            System.out.println("Tulosta (" + token + ", '" + c + "').");
-            System.out.println("    Lisää sana \"" + key + "\" koodilla " + nextToken + " sanakirjaan.");
-
-            key = "";
-            lastToken = 0;
+            int nextToken = dict.insert(key + (char)symbol);
+            System.out.println("Tulosta (" + lastToken + ", '" + (char)symbol + "').");
+            System.out.println("    Lisää sana \"" + (key + (char)symbol) + "\" koodilla " + nextToken + " sanakirjaan.");
         }
     }
 }
