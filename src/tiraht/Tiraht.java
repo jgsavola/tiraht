a9a5758a73dd2b4c8ebb093876a485368a3301e1
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-05-20 21:41:18 +0300

    Käytä LZ78Encoder- ja LZ78Decoder-luokkia pääohjelmassa

--- src/tiraht/Tiraht.java
+++ src/tiraht/Tiraht.java
@@ -1,7 +1,7 @@
 package tiraht;
 
 import java.io.IOException;
-import java.io.StringReader;
+import java.util.ArrayList;
 
 public class Tiraht {
     public static void main(String[] args) throws IOException {
@@ -9,27 +9,17 @@ public class Tiraht {
          * Esimerkki kirjasta Fundamental Data Compression, s. 137-138.
          */
         String sourceStr = "a date at a date";
-        StringDict dict = new StringDictWithHashMap();
-        StringReader reader = new StringReader(sourceStr);
-        int symbol;
-        while ((symbol = reader.read()) != -1) {
-            String key = "";
-            int lastToken = 0;
-            int token = 0;
-            while ((token = dict.search(key + (char)symbol)) != -1) {
-                int nextSymbol = reader.read();
-                if (nextSymbol == -1)
-                    break;
-                key += (char)symbol;
-                lastToken = token;
-                symbol = nextSymbol;
-            }
-            /**
-             * Avainta ei löytynyt sanakirjasta.
-             */
-            int nextToken = dict.insert(key + (char)symbol);
-            System.out.println("Tulosta (" + lastToken + ", '" + (char)symbol + "').");
-            System.out.println("    Lisää sana \"" + (key + (char)symbol) + "\" koodilla " + nextToken + " sanakirjaan.");
-        }
+        LZ78Encoder encoder = new LZ78Encoder();
+        ArrayList<PairToken> tokens = encoder.encode(sourceStr);
+        for (PairToken token : tokens)
+            System.out.println("Tulosta (" + token.index + ", '" + token.c + "').");
+
+        LZ78Decoder decoder = new LZ78Decoder();
+        String decodedStr = decoder.decode(tokens);
+        System.out.println("purettu: " + decodedStr);
+        if (sourceStr.equals(decodedStr))
+            System.out.println("    ==> OK");
+        else
+            System.out.println("    ==> virhe");
     }
 }
