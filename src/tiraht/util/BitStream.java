f63782bd59f1d5859c624a3f5848c31930b5df29
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-06-01 13:41:29 +0300

    BitStream: tee uusi luokka bittivirran kirjoittamiseen
    Yksinkertainen toteutus: yhden tavun puskuri, johon kirjoitettan yksi
    tavu kerrallaan. Kun puskuri täyttyy, tavu kirjoitetaan
    tulostusvirtaan.

---
+++ src/tiraht/util/BitStream.java
@@ -0,0 +1,73 @@
+package tiraht.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Tämä luokka kirjoittaa bittejä tavuvirtaan käyttäen tavupuskuria.
+ *
+ * Bitit kirjoitetaan järjestyksessä merkitsevimmästä bitistä vähiten
+ * merkitsevään. Jos bittien määrä ei ole jaollinen 8:lla, viimeisen
+ * tavun vähiten merkitsevät bitit ovat nollia.
+ *
+ * @author jgsavola
+ */
+public class BitStream {
+    private byte buffer;
+    private int position;
+    private OutputStream os;
+
+    /**
+     * Luo uusi bittivirta.
+     *
+     * @param os Tulostusvirta, johon bitit kirjoitetaan.
+     */
+    public BitStream(OutputStream os) {
+        this.buffer = 0;
+        this.position = 0;
+        this.os = os;
+    }
+
+    /**
+     * Kirjoita yksi bitti bittivirtaan.
+     *
+     * @param bit Kirjoitettava bitti: true => 1, false => 0.
+     * @throws IOException 
+     */
+    public void writeBit(boolean bit) throws IOException {
+        /**
+         * Puskurin loppupään jäännösbittien oletetaan olevan nollia,
+         * joten vain ykköset lisätään puskuriin.
+         */
+        if (bit)
+            buffer |= 1 << (7 - position);
+
+        position++;
+        if (position == 8) {
+            os.write(buffer);
+            position = 0;
+            buffer = 0;
+        }
+    }
+
+    /**
+     * Kirjoita viimeinen, ei-täysi tavu tulostusvirtaan.
+     *
+     * <p>
+     * Huom! Jos tämän metodin kutsumisen jälkeen kirjoitetan lisää bittejä
+     * tai kutsutaan <code>flush</code>-metodia, bittivirtaan tulee aukkoja.
+     *
+     * <p>
+     * Puskuri alustetaan jokaisella <code>flush</code>-kutsulla.
+     *
+     * @throws IOException
+     */
+    public void flush() throws IOException {
+        if (position > 0) {
+            os.write(buffer);
+            position = 0;
+        }
+        os.flush();
+        buffer = 0;
+    }
+}
---
+++ test/tiraht/util/BitStreamTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiraht.util;
+
+import java.io.ByteArrayOutputStream;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.*;
+
+/**
+ *
+ * @author jgsavola
+ */
+public class BitStreamTest {
+    public BitStreamTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of writeBit method, of class BitStream.
+     */
+    @Test
+    public void testWriteBit() throws Exception {
+        System.out.println("writeBit");
+        byte[] expected = {(byte)0xaa};
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        BitStream instance = new BitStream(bos);
+
+        instance.writeBit(true);
+        instance.writeBit(false);
+        instance.writeBit(true);
+        instance.writeBit(false);
+        instance.writeBit(true);
+        instance.writeBit(false);
+        instance.writeBit(true);
+        instance.writeBit(false);
+        instance.flush();
+
+        assertArrayEquals("Bittien kirjoittaminen toimii.", expected, bos.toByteArray());
+    }
+
+    /**
+     * Test of flush method, of class BitStream.
+     */
+    @Test
+    public void testFlush() throws Exception {
+        System.out.println("flush");
+        byte[] expected = {(byte)0xa0};
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        BitStream instance = new BitStream(bos);
+
+        instance.writeBit(true);
+        instance.writeBit(false);
+        instance.writeBit(true);
+        instance.writeBit(false);
+        instance.flush();
+
+        assertArrayEquals("flush-metodi toimii.", expected, bos.toByteArray());
+    }
+}
