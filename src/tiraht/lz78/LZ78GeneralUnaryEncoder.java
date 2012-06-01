7d20f12993560a8ac71d515b806b9b20b9a71de1
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-06-01 14:12:20 +0300

    LZ78GeneralUnaryEncoder: muuta kirjoittamaan binäärikoodattua dataa

--- src/tiraht/lz78/LZ78GeneralUnaryEncoder.java
+++ src/tiraht/lz78/LZ78GeneralUnaryEncoder.java
@@ -2,39 +2,54 @@ package tiraht.lz78;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import tiraht.util.BitStream;
 import tiraht.util.GeneralUnaryEncoder;
 
 /**
- * Testaa koodaamista "General Unary" -koodilla.
+ * Koodaa LZ78-kompressoitua virtaa käyttäen
+ * <code>GeneralUnaryEncoder</code>-kooderia.
  *
- * Lähde: Salomon, David: Data Compression. The Complete Reference.
+ * Lähde: Salomon, David: Data Compression. The Complete Reference, 2007.
  *
  * @author jgsavola
  */
 public class LZ78GeneralUnaryEncoder implements LZ78TokenWriter {
     private GeneralUnaryEncoder unaryEncoder;
-    private OutputStream os;
-    private long sumBits = 0;
-    
+    private BitStream bs;
+
+    /**
+     * Luo "General Unary" -kooderi.
+     *
+     * Parametrit <code>start</code>, <code>step</code>, <code>stop</code>
+     * kannattaa valita koodattavien lukujen jakauman mukaan. Erilaisten
+     * koodien määrä saadaan kaavasta
+     *
+     * <pre>
+     *   (2^(stop+step) - 2^start) / (2^step - 1).
+     * </pre>
+     *
+     * Luokka kirjoittaa koodatut kokonaisluvut suoraan annettuun
+     * tulostusvirtaan käyttäen <code>BitStream</code>-luokkaa.
+     *
+     * @param os Tulostusvirta, johon koodi kirjoitetaan.
+     * @param start Lyhimmän bittijonon pituus.
+     * @param step Bittien määrä, jolla bittijonon pituutta kasvatetaan.
+     * @param stop Pisimmän bittijonon pituus
+     */
     public LZ78GeneralUnaryEncoder(OutputStream os, int start, int step, int stop) {
-        this.os = os;
-        unaryEncoder = new GeneralUnaryEncoder(start, step, stop);
+        this.bs = new BitStream(os);
+        unaryEncoder = new GeneralUnaryEncoder(bs, start, step, stop);
     }
 
     @Override
     public void writeLZ78Token(LZ78Token token) throws IOException {
-        String u = unaryEncoder.encode(token.getPrefixIndex());
-        sumBits += u.length() - 1 + 8;
-        String s = u + "\t(" + token.getSuffixByte() + ")\t" + token.getPrefixIndex() + " len: " + (u.length() - 1) + ", sum: " + (sumBits / 8) + "\n";
-        try {
-            os.write(s.getBytes("US-ASCII"));
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(LZ78GeneralUnaryEncoder.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        unaryEncoder.encode(token.getPrefixIndex());
+        for (int b = 7; b >= 0; b--)
+            bs.writeBit((token.getSuffixByte() & (1 << b)) != 0);
     }
 
-    
+    @Override
+    public void flush() throws IOException {
+        bs.flush();
+    }
 }
