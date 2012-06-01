2ce0ecfbcbfc7b1e490c57bff15d30efcfaaadae
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-06-01 14:43:09 +0300

    Lisää pääohjelmaan komentoriviparseri
    Käyttö:
    
      tiraht [optiot] tiedosto1 [tiedosto2 ...]
    
    Optiot:
    
      --dict-size dictSize
          Sanakirjan maksimikoko
      --dict-fill-up-strategy strategy
          Strategia sanakirjan täyttymiselle: reset, freeze, donothing
      --general-unary-params start step stop
          General Unary -koodauksen parametrit

--- src/tiraht/Tiraht.java
+++ src/tiraht/Tiraht.java
@@ -1,11 +1,5 @@
 package tiraht;
 
-import tiraht.lz78.LZ78FromIteratorDecoder;
-import tiraht.lz78.LZ78ByteTrieCompressor;
-import tiraht.lz78.LZ78ToArrayListEncoder;
-import tiraht.lz78.LZ78Token;
-import tiraht.lz78.LZ78HashMapDecompressor;
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,36 +7,72 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import tiraht.lz78.*;
+import tiraht.lz78.LZ78ByteTrieCompressor;
+import tiraht.lz78.LZ78GeneralUnaryEncoder;
 
 public class Tiraht {
+    /**
+     * Käytä pientä sanakirjaa, jotta muistinkäyttö ei olisi liian suuri.
+     */
+    static int dictSize = 65536;
+
+    /**
+     * Sanakirjan täyttyessä aloita alusta tyhjällä sanakirjalla.
+     */
+    static LZ78ByteTrieCompressor.DictFillUpStrategy dictFillUpStrategy = LZ78ByteTrieCompressor.DictFillUpStrategy.Reset;
+
+    /**
+     * Arvot (start=12, step=1) näyttävät tuottavan kohtalaisen tuloksen
+     * tiedostoilla, joiden koko on sadoista kymmenistä kiloista muutamaan
+     * megatavuun, kun sanakirjan koko on luokkaa 100000.
+     *
+     * Suurin koodattava koodi olkoon 30 bittiä, jotta ei tule
+     * kokonaislukuylivuotoa.
+     */
+    static int start = 12;
+    static int step = 1;
+    static int stop = 30;
+
+    static ArrayList<String> inputFilenames = new ArrayList<String>();
+
     public static void main(String[] args) {
-        System.out.println("Tavuilla (trie):");
-        testWithByteTrie(args);
+        parseCommandLine(args);
+        testWithByteTrie();
     }
 
-    public static void testWithByteTrie(String[] args) {
-        int start;
-        int step;
-        int stop;
-        
-        LinkedList<String> la = new LinkedList<String>();
+    private static void parseCommandLine(String[] args) {
         for (int i = 0; i < args.length; i++) {
-            la.add(args[i]);
+            if ("--dict-size".equals(args[i])) {
+                dictSize = Integer.parseInt(args[++i]);
+            } else if ("--dict-fill-up-strategy".equals(args[i])) {
+                String opt = args[++i];
+                if ("reset".equalsIgnoreCase(opt))
+                    dictFillUpStrategy = LZ78ByteTrieCompressor.DictFillUpStrategy.Reset;
+                else if ("donothing".equalsIgnoreCase(opt))
+                    dictFillUpStrategy = LZ78ByteTrieCompressor.DictFillUpStrategy.DoNothing;
+                else if ("freeze".equalsIgnoreCase(opt))
+                    dictFillUpStrategy = LZ78ByteTrieCompressor.DictFillUpStrategy.Freeze;
+                else
+                    throw new IllegalArgumentException("Tuntematon sanakirjastrategia: " + opt);
+            } else if ("--general-unary-params".equals(args[i])) {
+                start = Integer.parseInt(args[++i]);
+                step = Integer.parseInt(args[++i]);
+                stop = Integer.parseInt(args[++i]);
+            } else {
+                inputFilenames.add(args[i]);
+            }
         }
-        start = Integer.parseInt(la.removeFirst());
-        step  = Integer.parseInt(la.removeFirst());
-        stop  = Integer.parseInt(la.removeFirst());
-        
-        for (String filename : la) {
+    }
+
+    public static void testWithByteTrie() {
+        for (String filename : inputFilenames) {
             try {
                 FileInputStream reader = new FileInputStream(filename);
 
                 /**
                  * Kompressointi
                  */
-
-                LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(16384, LZ78ByteTrieCompressor.DictFillUpStrategy.Reset);
+                LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(dictSize, dictFillUpStrategy);
                 LZ78GeneralUnaryEncoder encoder = new LZ78GeneralUnaryEncoder(System.out, start, step, stop);
                 compressor.compress(reader, encoder);
 
