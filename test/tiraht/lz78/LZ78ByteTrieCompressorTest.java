0be0ca121622e3fa14ba70878782702f35adc450
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-06-04 12:24:59 +0300

    LZ78ByteTrieCompressorTest: testaa sanakirjastrategioita

--- test/tiraht/lz78/LZ78ByteTrieCompressorTest.java
+++ test/tiraht/lz78/LZ78ByteTrieCompressorTest.java
@@ -7,6 +7,8 @@ import java.io.ByteArrayInputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.*;
 
 /**
@@ -71,7 +73,7 @@ public class LZ78ByteTrieCompressorTest {
 
         instance.compress(is, encoder);
 
-        assertEquals("Kompressio tuottaa odotetut LZ78-koodit.", 
+        assertEquals("Kompressio tuottaa odotetut LZ78-koodit.",
                 pairListToString(example1ExpResult),
                 pairListToString(encoder.getTokens()));
     }
@@ -85,9 +87,209 @@ public class LZ78ByteTrieCompressorTest {
 
         instance.compress(example1Bytes, encoder);
 
-        assertEquals("Kompressio tuottaa odotetut LZ78-koodit.", 
+        assertEquals("Kompressio tuottaa odotetut LZ78-koodit.",
                 pairListToString(example1ExpResult),
                 pairListToString(encoder.getTokens()));
+    }
+
+    /**
+     * Test of compress method, of class LZ78ByteTrieCompressor.
+     */
+    @Test
+    public void testDictFillUpResetIllegalSize() throws Exception {
+        String sourceStr = "a date with a date";
+        byte[] sourceBytes = sourceStr.getBytes("US-ASCII");
+
+        try {
+            LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(0, LZ78ByteTrieCompressor.DictFillUpStrategy.Reset);
+            fail("Sanakirjan koko 0 tai < -1 on virhe");
+        } catch (IllegalArgumentException ex) {
+            assertTrue("Sanakirjan koko 0 tai < -1 on virhe", true);
+        }
+        try {
+            LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(-2, LZ78ByteTrieCompressor.DictFillUpStrategy.Reset);
+            fail("Sanakirjan koko 0 tai < -1 on virhe");
+        } catch (IllegalArgumentException ex) {
+            assertTrue("Sanakirjan koko 0 tai < -1 on virhe", true);
+        }
+    }
+
+    /**
+     * Test of compress method, of class LZ78ByteTrieCompressor.
+     */
+    @Test
+    public void testDictFillUpResetSize1() throws Exception {
+        String sourceStr = "a date with a date";
+        byte[] sourceBytes = sourceStr.getBytes("US-ASCII");
+
+        {
+            LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(1, LZ78ByteTrieCompressor.DictFillUpStrategy.Reset);
+            System.out.println("compress(\"" + sourceStr + "\"::byte[])");
+
+            compressor.compress(sourceBytes, encoder);
+
+            System.out.println(pairListToCode(encoder.getTokens()));
+
+            assertEquals("Kompressio tuottaa odotetut LZ78-koodit.",
+                    pairListToString(expectedWithResetAndDictSizes1And2()),
+                    pairListToString(encoder.getTokens()));
+        }
+    }
+
+    /**
+     * Test of compress method, of class LZ78ByteTrieCompressor.
+     */
+    @Test
+    public void testDictFillUpResetSize2() throws Exception {
+        String sourceStr = "a date with a date";
+        byte[] sourceBytes = sourceStr.getBytes("US-ASCII");
+
+        {
+            LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(2, LZ78ByteTrieCompressor.DictFillUpStrategy.Reset);
+            System.out.println("compress(\"" + sourceStr + "\"::byte[])");
+
+            compressor.compress(sourceBytes, encoder);
+
+            System.out.println(pairListToCode(encoder.getTokens()));
+
+            assertEquals("Kompressio tuottaa odotetut LZ78-koodit.",
+                    pairListToString(expectedWithResetAndDictSizes1And2()),
+                    pairListToString(encoder.getTokens()));
+        }
+    }
+
+    /**
+     * Test of compress method, of class LZ78ByteTrieCompressor.
+     */
+    @Test
+    public void testDictFillUpResetSize5() throws Exception {
+        String sourceStr = "a date with a date";
+        byte[] sourceBytes = sourceStr.getBytes("US-ASCII");
+
+        {
+            LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(5, LZ78ByteTrieCompressor.DictFillUpStrategy.Reset);
+            System.out.println("compress(\"" + sourceStr + "\"::byte[])");
+
+            compressor.compress(sourceBytes, encoder);
+
+            System.out.println(pairListToCode(encoder.getTokens()));
+
+            assertEquals("Kompressio tuottaa odotetut LZ78-koodit.",
+                    pairListToString(expectedWithResetAndDictSize5()),
+                    pairListToString(encoder.getTokens()));
+        }
+    }
+
+    /**
+     * Test of compress method, of class LZ78ByteTrieCompressor.
+     */
+    @Test
+    public void testDictFillUpFreezeSize1() throws Exception {
+        String sourceStr = "a date with a date";
+        byte[] sourceBytes = sourceStr.getBytes("US-ASCII");
+
+        {
+            LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(1, LZ78ByteTrieCompressor.DictFillUpStrategy.Freeze);
+            System.out.println("compress(\"" + sourceStr + "\"::byte[])");
+
+            compressor.compress(sourceBytes, encoder);
+
+            System.out.println(pairListToCode(encoder.getTokens()));
+
+            assertEquals("Kompressio tuottaa odotetut LZ78-koodit.",
+                    pairListToString(expectedWithResetAndDictSizes1And2()),
+                    pairListToString(encoder.getTokens()));
+        }
+    }
+
+    /**
+     * Test of compress method, of class LZ78ByteTrieCompressor.
+     */
+    @Test
+    public void testDictFillUpFreezeSize2() throws Exception {
+        String sourceStr = "a date with a date";
+        byte[] sourceBytes = sourceStr.getBytes("US-ASCII");
+
+        {
+            LZ78ByteTrieCompressor compressor = new LZ78ByteTrieCompressor(2, LZ78ByteTrieCompressor.DictFillUpStrategy.Freeze);
+            System.out.println("testDictFillUpFreezeSize2: compress(\"" + sourceStr + "\"::byte[])");
+
+            compressor.compress(sourceBytes, encoder);
+
+            System.out.println(pairListToCode(encoder.getTokens()));
+
+            assertEquals("Kompressio tuottaa odotetut LZ78-koodit.",
+                    pairListToString(expectedWithFreezeAndDictSize2()),
+                    pairListToString(encoder.getTokens()));
+        }
+    }
+
+    private ArrayList<LZ78Token> expectedWithResetAndDictSizes1And2() {
+        ArrayList<LZ78Token> tokens = new ArrayList<LZ78Token>();
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)100));
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)101));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)119));
+        tokens.add(new LZ78Token(0, (byte)105));
+        tokens.add(new LZ78Token(0, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)104));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)100));
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)101));
+
+        return tokens;
+    }
+
+    private ArrayList<LZ78Token> expectedWithResetAndDictSize5() {
+        ArrayList<LZ78Token> tokens = new ArrayList<LZ78Token>();
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)100));
+        tokens.add(new LZ78Token(1, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)101));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)119));
+        tokens.add(new LZ78Token(0, (byte)105));
+        tokens.add(new LZ78Token(0, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)104));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)100));
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)101));
+
+        return tokens;
+    }
+
+    private ArrayList<LZ78Token> expectedWithFreezeAndDictSize2() {
+        ArrayList<LZ78Token> tokens = new ArrayList<LZ78Token>();
+        tokens.add(new LZ78Token(0, (byte)97));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)100));
+        tokens.add(new LZ78Token(1, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)101));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)119));
+        tokens.add(new LZ78Token(0, (byte)105));
+        tokens.add(new LZ78Token(0, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)104));
+        tokens.add(new LZ78Token(0, (byte)32));
+        tokens.add(new LZ78Token(1, (byte)32));
+        tokens.add(new LZ78Token(0, (byte)100));
+        tokens.add(new LZ78Token(1, (byte)116));
+        tokens.add(new LZ78Token(0, (byte)101));
+
+        return tokens;
     }
 
     /**
@@ -100,6 +302,20 @@ public class LZ78ByteTrieCompressorTest {
         String result = "";
         for (LZ78Token token : tokens)
             result += token + "\n";
+
+        return result;
+    }
+
+    /**
+     * Käytetään testauskoodin generoimiseen.
+     *
+     * @param tokens Kompression tuloslista.
+     * @return Tekstimuotoinen esitys.
+     */
+    private String pairListToCode(ArrayList<LZ78Token> tokens) {
+        String result = "ArrayList<LZ78Token> tokens = new ArrayList<LZ78Token>();\n";
+        for (LZ78Token token : tokens)
+            result += "tokens.add(new LZ78Token(" + token.getPrefixIndex() + ", (byte)" + (int)token.getSuffixByte() + "));\n";
 
         return result;
     }
