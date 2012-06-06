178ddf096e90000f806d2a4176d3851630b61fe6
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-06-06 14:08:21 +0300

    ByteTrie: lisää javadoc ja poista kommentoitua koodia

--- src/tiraht/dict/ByteTrie.java
+++ src/tiraht/dict/ByteTrie.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.TreeMap;
 
 /**
+ * Trie-rakenne, joka käyttää avaimena tavujonoa.
  *
+ * Tämä toteutus käyttää lapsisolmujen tallentamiseen TreeMap-rakennetta.
  *
  * @author jonne
  */
@@ -14,24 +16,43 @@ public class ByteTrie<E> implements Trie {
     private boolean isValid;
     private E value;
 
-    //private ArrayList<ByteTrie<E>> children;
     private TreeMap<Byte, ByteTrie<E>> children;
 
+    /**
+     * Luo uusi <code>ByteTrie</code> tulokaaren koodilla <code>nodeKey</code>.
+     *
+     * @param nodeKey Avaimen osa (tavu), jolla tähän solmuun on tultu.
+     */
     public ByteTrie(byte nodeKey) {
         this();
         this.nodeKey = nodeKey;
     }
 
+    /**
+     * Luo uusi <code>ByteTrie</code>.
+     */
     public ByteTrie() {
         this.isValid = false;
-        //this.children = new ArrayList<ByteTrie<E>>();
         this.children = new TreeMap<Byte, ByteTrie<E>>();
     }
 
+    /**
+     * Anna solmun arvo.
+     *
+     * Jos solmulla ei ole arvoa, palauta <code>null</code>.
+     *
+     * @return Solmun arvo.
+     */
     public E getValue() {
         return value;
     }
 
+    /**
+     * Lisää arvo <code>value</code> avaimella <code>key</code>.
+     *
+     * @param key Avain tavuiteraattorina.
+     * @param value Lisättävä arvo.
+     */
     public void insert(Iterator<Byte> key, E value) {
         if (!key.hasNext()) {
             this.value = value;
@@ -48,10 +69,22 @@ public class ByteTrie<E> implements Trie {
         child.insert(key, value);
     }
 
+    /**
+     * Lisää arvo <code>value</code> avaimella <code>key</code>.
+     *
+     * @param key Avain tavulistana.
+     * @param value Lisättävä arvo.
+     */
     public void insert(List<Byte> key, E value) {
         insert(key.listIterator(), value);
     }
 
+    /**
+     * Lisää arvo <code>value</code> avaimella <code>key</code>.
+     *
+     * @param key Avain tavuna.
+     * @param value Lisättävä arvo.
+     */
     public void insert(Byte key, E value) {
         ByteTrie<E> child = findChild(key);
         if (child == null) {
@@ -64,30 +97,43 @@ public class ByteTrie<E> implements Trie {
         child.value = value;
     }
 
-//    private ByteTrie<E> findChild(Byte b) {
-//        for (ByteTrie<E> child : children)
-//            if (child.nodeKey == b)
-//                return child;
-//
-//        return null;
-//    }
-//
-//    private void addChild(ByteTrie<E> child) {
-//        children.add(child);
-//    }
-
-    private ByteTrie<E> findChild(Byte b) {
-        return children.get(b);
+    /**
+     * Hae solmun lapsi avaimella <code>key</code>.
+     *
+     * @param key Avain tavumuodossa.
+     * @return Lapsisolmu tai <code>null</code>.
+     */
+    private ByteTrie<E> findChild(Byte key) {
+        return children.get(key);
     }
 
+    /**
+     * Lisää solmuun lapsisolmu <code>child</code>.
+     *
+     * Avaimena käytetään lapsisolmun attribuuttia <code>nodeKey</code>.
+     *
+     * @param child Lisättävä lapsisolmu.
+     */
     private void addChild(ByteTrie<E> child) {
         children.put(child.nodeKey, child);
     }
 
+    /**
+     * Etsi solmu avaimella <code>key</code> ja palauta löydetyn solmun arvo.
+     *
+     * @param key Avain tavulistana.
+     * @return Löydetty arvo tai <code>null</code>.
+     */
     public E search(List<Byte> key) {
         return search(key.listIterator());
     }
 
+    /**
+     * Etsi solmu avaimella <code>key</code> ja palauta löydetyn solmun arvo.
+     *
+     * @param key Avain tavuiteraattorina.
+     * @return Löydetty arvo tai <code>null</code>.
+     */
     public E search(Iterator<Byte> key) {
         if (!key.hasNext()) {
             if (isValid)
@@ -103,6 +149,14 @@ public class ByteTrie<E> implements Trie {
         return child.search(key);
     }
 
+    /**
+     * Etsi solmu avaimella <code>key</code> ja palauta löydetty solmu.
+     *
+     * Tämä metodi soveltuu iteratiiviseen etsimiseen ja lisäämiseen.
+     *
+     * @param key Avain tavuiteraattorina.
+     * @return Löydetty solmu tai <code>null</code>.
+     */
     public ByteTrie<E> retrieve(Iterator<Byte> key) {
         if (!key.hasNext()) {
             if (isValid)
@@ -118,6 +172,14 @@ public class ByteTrie<E> implements Trie {
         return child.retrieve(key);
     }
 
+    /**
+     * Etsi solmu avaimella <code>key</code> ja palauta löydetty solmu.
+     *
+     * Tämä metodi soveltuu iteratiiviseen etsimiseen ja lisäämiseen.
+     *
+     * @param key Avain tavuna.
+     * @return Löydetty solmu tai <code>null</code>.
+     */
     public ByteTrie<E> retrieve(Byte key) {
         return findChild(key);
     }
