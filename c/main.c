2d637914620434779e27911714544d3c1350c44c
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-06-15 14:02:04 +0300

    tiraht_c: siivoa main

--- c/main.c
+++ c/main.c
@@ -5,10 +5,6 @@
 
 int main(int argc, char **argv)
 {
-//	lz78_compress_string("a date at a date");
-
-//	lz78_compress_string("Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do:  once or twice she had peeped into the book her sister was reading, but it had nopictures or conversations in it, `and what is the use of a book,'thought Alice `without pictures or conversation?'");
-
 	for (int i = 1; i < argc; i++) {
 		FILE *f = fopen(argv[i], "rb");
 		if (!f) {
@@ -20,11 +16,3 @@ int main(int argc, char **argv)
 
 	exit(0);
 }
-
-/**
-
-a 1
-  2
-d 3
-
-**/
